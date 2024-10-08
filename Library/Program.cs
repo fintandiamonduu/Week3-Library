dae108c6e52c395487d7a66483d1de612a570702
Fintan Diamond <Diamond-F@ulster.ac.uk>
2024-10-08 10:13:55 +0100

    First commit

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+    int numOfPages;
+
+    void DisplayInfo()
+    {
+        //output the book info to the console 
+        Console.WriteLine("Book information");
+        Console.WriteLine("...................");
+        Console.WriteLine($"book title: {Title}");
+        Console.WriteLine($"Author title: {Author}");
+        Console.WriteLine($"ISBN number: {ISBN}");
+        Console.WriteLine($"Number of pages: {numOfPages}");
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        //create a new instance of the book class 
+        Book book = new Book();
+
+        book.Title = "C# for beginners";
+        book.Author = "Bill Gates";
+        book.ISBN = "767269379";
+        book.numOfPages = 200;
+
+        Book book2 = new Book();
+        book2.Title = "C# methods and classes";
+        book2.Author = "Microsoft";
+        book2.ISBN = "3767620121";
+        book2.numOfPages = 50;
+
+        // output book info to console 
+        book.DisplayInfo();
+        book2.DisplayInfo();
+
+      
+    }
+}
